3cd03a0ec3f4393a780910e6cd2bbd7b32d1c58e
linus-ship-it187 <hollmannlinus@gmail.com>
2026-09-19 18:30:19 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase-Konfiguration für dieses Projekt (jeopardyvonflixo).
+// apiKey & co. sind für Web-Apps öffentlich sichtbar, das ist normal und kein Sicherheitsproblem.
+const firebaseConfig = {
+  apiKey: "AIzaSyCDoZCuIQ-y785px-X-oaMq1SKnaH1WZTk",
+  authDomain: "jeopardyvonflixo.firebaseapp.com",
+  databaseURL: "https://jeopardyvonflixo-default-rtdb.firebaseio.com",
+  projectId: "jeopardyvonflixo",
+  storageBucket: "jeopardyvonflixo.firebasestorage.app",
+  messagingSenderId: "372958581592",
+  appId: "1:372958581592:web:be20f84b139268af5a8b2d",
+  measurementId: "G-5CBWM33M5M"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
